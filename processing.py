3e0b3d9f85132400b15274bcd40ba3b00ac97e71
andreasiden <andreasiden1@gmail.com>
2018-11-26 17:56:09 +0100

    bug fix

--- processing.py
+++ processing.py
@@ -1,18 +1,21 @@
 from pyspark import SparkContext
 
 
-# import findspark
-# findspark.init()
-
 # Class for pyspark processing
 class Processing:
+    """
+        Constructor that creates or gets a SparkContext object.
+    """
 
-    # Constructor that creates or gets a SparkContext object.
     def __init__(self):
         self.sc = SparkContext.getOrCreate()
 
-    # takes a csvfile and a number as arguments. Finds the numberOfWords most popular words used in tweets stored in csvfile
-    # and returns a list with tuples as elements. The tuples are frequencies and words.
+    """
+        **************************************************************************************************************
+        takes a csvfile and a number as arguments. Finds the numberOfWords most popular words used in tweets stored in 
+        csvfile and returns a list with tuples as elements. The tuples are frequencies and words.
+    """
+
     def wordcounter(self, csvfile, numberOfWords):
         lines = self.sc.textFile(csvfile)
         lines_nonempty = lines.filter(lambda x: len(x) > 0)
@@ -22,8 +25,13 @@ class Processing:
         res = wordcounts.take(numberOfWords)
         return res
 
-    # takes a csvfile with twitter texts and a number as arguments. Finds the numberOfWords most popular combination of two
-    # words in sequence (bigrams), and returns these in a list with tuples as elements. The tuples are frequencies and bigrams.
+    """
+        *************************************************************************************************************
+        takes a csvfile with twitter texts and a number as arguments. Finds the numberOfWords most popular combination 
+        of two words in sequence (bigrams), and returns these in a list with tuples as elements. The tuples are 
+        frequencies and bigrams.
+    """
+
     def bigram(self, csvfile, numberOfWords):
         sentences = self.sc.textFile(csvfile) \
             .glom() \
@@ -39,13 +47,21 @@ class Processing:
         res = (freq_bigrams.take(numberOfWords))
         return res
 
-    # Function that taks a list as arguments and returns the most frequent element in that list
+    """
+        *****************************************************************************************
+        Function that taks a list as arguments and returns the most frequent element in that list
+    """
+
     def most_common(self, list):
         return max(set(list), key=list.count)
 
-    #Takes a list as argument. Uses a SparkContext object to find all elements in the list that contains a string,
-    # in this example, "RT" - to find all elements in the list that contains the string. All elements containing the
-    # string are returned.
+    """
+        ************************************************************************************************************
+        Takes a list as argument. Uses a SparkContext object to find all elements in the list that contains a string,
+        in this example, "RT" - to find all elements in the list that contains the string. All elements containing the
+        string are returned.
+    """
+
     def retweets(self, list):
         words = self.sc.parallelize(list)
         words_filter = words.filter(lambda x: 'RT' in x)
@@ -53,6 +69,10 @@ class Processing:
         # res = self.most_common(filtered)
         return filtered
 
-    #Stops the running of a SparkContext object.
+    """
+        ******************************************
+        Stops the running of a SparkContext object.
+    """
+
     def stopspark(self):
         self.sc.stop()
